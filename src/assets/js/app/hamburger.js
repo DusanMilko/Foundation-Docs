2556df3b6c042d661a18823db291f35cc9a252e3
Dusan Milko <dusan.milko@rokkan.com>
2015-04-01 16:59:54 -0400

    close hamburger on click out of

--- src/assets/js/app/hamburger.js
+++ src/assets/js/app/hamburger.js
@@ -14,4 +14,21 @@ module.exports = function() {
 
     return false;
   });
+
+  // On click outside 
+  $(document).mouseup(function (e) {
+    var hamburgerCont = $('.js-hamburger-cont');
+    var hamburgerBtn = $('.js-hamburger');
+
+    if (
+      (!hamburgerCont.is(e.target) // if the target of the click isn't the container...
+      && hamburgerCont.has(e.target).length === 0) // ... nor a descendant of the container
+      &&
+      (!hamburgerBtn.is(e.target) // if the target of the click isn't the container...
+      && hamburgerBtn.has(e.target).length === 0) // ... nor a descendant of the container
+    ){
+      $('.js-hamburger').removeClass('is-active');
+      $('.js-hamburger-cont').removeClass('is-active');
+    }
+  });
 }
