3d53290235c5a993a246238db6ba509adb6dfcc4
Dusan Milko <dusan.milko@rokkan.com>
2015-05-29 15:48:57 -0400

    updating docs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,5 @@
 var basic = require('./app/basic')();
 var scrollTo = require('./app/scroll-to')();
 var hamburger = require('./app/hamburger')();
+
+var docs_side_nav = require('./app/docs-side-nav')();
