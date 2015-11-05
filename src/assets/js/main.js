61f82473072fa668ec7a8a86f3132b4d57630aa4
Dusan Milko <dusan.milko@rokkan.com>
2015-11-05 15:28:06 -0500

    adding plugin

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,9 @@
+var globals = require('./app/globals')();
 var basic = require('./app/basic')();
 var scrollTo = require('./app/scroll-to')();
 var hamburger = require('./app/hamburger')();
 
 var docs_side_nav = require('./app/docs-side-nav')();
 
+o(plugins);
 
